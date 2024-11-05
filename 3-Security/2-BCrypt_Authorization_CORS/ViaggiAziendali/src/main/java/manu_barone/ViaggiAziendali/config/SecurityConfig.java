a0e18777ad984913522a9ebfd60e8d34ddd77437
Manuel Barone <baronemanuel05@gmail.com>
2024-11-05 17:37:57 +0100

    autorizzazioni

---
+++ 3-Security/2-BCrypt_Authorization_CORS/ViaggiAziendali/src/main/java/manu_barone/ViaggiAziendali/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package manu_barone.ViaggiAziendali.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
+        httpSecurity.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+        httpSecurity.sessionManagement(httpSecuritySessionManagementConfigurer ->
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.requestMatchers("/**").permitAll());
+
+        httpSecurity.cors(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConf() {
+        CorsConfiguration conf = new CorsConfiguration();
+        conf.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500","http://127.0.0.1:5500/"));
+        conf.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        conf.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));
+        conf.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource s = new UrlBasedCorsConfigurationSource();
+        s.registerCorsConfiguration("/**", conf);
+        return s;
+    }
+
+    @Bean
+    PasswordEncoder getBCrypt(){
+        return new BCryptPasswordEncoder(16);
+    }
+
+
+}
